6eab7ee6ff42bb2fe7c4f89865c633f7ac6a8751
ivaylokanov <kanov.ivo@gmail.com>
2017-05-28 00:16:43 +0300

    Java

---
+++ a_interfacesAndAbstraction/a_interfacesAndAbstraction_exercise/src/pr07_foodShortage/Main.java
@@ -0,0 +1,12 @@
+package pr07_foodShortage;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader());
+
+    }
+}
